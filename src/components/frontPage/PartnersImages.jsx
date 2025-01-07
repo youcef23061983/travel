cbb2f12651f543916d9b7ea792601730982b88b7
youcef23061983 <youcef.belcourtois@gmail.com>
2025-01-07 18:26:09 +0100

    3 commit

--- src/components/frontPage/PartnersImages.jsx
+++ src/components/frontPage/PartnersImages.jsx
@@ -1,92 +1,13 @@
-// import Image from "next/image";
-// import React from "react";
-
-// const PartnersImages = () => {
-//   return (
-//     <div className="marquee fadeout-horizontal" style={{ "--num-items": 7 }}>
-//       <div className="marquee-track">
-//         <div className="marquee-item" style={{ "--item-position": 1 }}>
-//           <Image
-//             src="/homepage/partners/1.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 1"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 2 }}>
-//           <Image
-//             src="/homepage/partners/2.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 2"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 3 }}>
-//           <Image
-//             src="/homepage/partners/3.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 3"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 4 }}>
-//           <Image
-//             src="/homepage/partners/4.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 4"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 5 }}>
-//           <Image
-//             src="/homepage/partners/5.png"
-//             width={40}
-//             className="img"
-//             height={40}
-//             alt="Partner 5"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 6 }}>
-//           <Image
-//             src="/homepage/partners/6.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 6"
-//           />
-//         </div>
-//         <div className="marquee-item" style={{ "--item-position": 7 }}>
-//           <Image
-//             src="/homepage/partners/7.png"
-//             width={40}
-//             height={40}
-//             alt="Partner 7"
-//             className="img"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PartnersImages;
-
-import React from "react";
 import { useLocale } from "next-intl";
 import Image from "next/image";
 
 const PartnersImages = () => {
   const locale = useLocale();
-  const isRTL = locale === "en";
+  const isRTL = locale === "ar";
 
   return (
     <div
-      // className={`marquee fadeout-horizontal ${isRTL ? "ltr" : "rtl"}`}
-      className="marquee fadeout-horizontal"
+      className={`marquee fadeout-horizontal ${isRTL ? "rtl" : "ltr"}`}
       style={{ "--num-items": 7 }}
     >
       <div className="marquee-track">
@@ -94,7 +15,12 @@ const PartnersImages = () => {
           <div
             key={index}
             className="marquee-item"
-            style={{ "--item-position": index + 1 }}
+            style={{
+              "--item-position": index + 1,
+              animation: `${
+                isRTL ? "marquee-move-rtl" : "marquee-move-ltr"
+              } var(--speed) linear infinite`,
+            }}
           >
             <Image
               src={`/homepage/partners/${index + 1}.png`}
@@ -111,45 +37,3 @@ const PartnersImages = () => {
 };
 
 export default PartnersImages;
-// import { useLocale, useTranslations } from "next-intl";
-// import Image from "next/image";
-
-// const PartnersImages = () => {
-//   const locale = useLocale();
-//   const isRTL = locale === "ar";
-//   const t = useTranslations("HomePage");
-
-//   const partnersImages = Array.isArray(t("Partners"))
-//     ? t("Partners")
-//     : [...Array(7)].map((_, index) => `/homepage/partners/${index + 1}.png`);
-
-//   console.log("partnersImages:", partnersImages); // Debugging
-
-//   return (
-//     <div
-//       className={`marquee fadeout-horizontal ${isRTL ? "rtl" : ""}`}
-//       // className="marquee fadeout-horizontal"
-//       style={{ "--num-items": partnersImages.length }}
-//     >
-//       <div className="marquee-track">
-//         {partnersImages.map((image, index) => (
-//           <div
-//             key={index}
-//             className={`marquee-item ${isRTL ? "marquee-item-rtl" : ""}`}
-//             style={{ "--item-position": index + 1 }}
-//           >
-//             <Image
-//               src={image}
-//               width={40}
-//               height={40}
-//               className="img"
-//               alt={`Partner ${index + 1}`}
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PartnersImages;
--- src/components/frontPage/Partnership2.jsx
+++ src/components/frontPage/Partnership2.jsx
@@ -1,101 +1,24 @@
-// import Image from "next/image";
-
-// const Partnership2 = () => {
-//   return (
-//     <div className="marquee fadeout-horizontal" style={{ "--num-items": 7 }}>
-//       <div className="marquee-track">
-//         <div className="marquee-item2" style={{ "--item-position": 1 }}>
-//           <Image
-//             src="/homepage/partners/1.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 1"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 2 }}>
-//           <Image
-//             src="/homepage/partners/2.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 2"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 3 }}>
-//           <Image
-//             src="/homepage/partners/3.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 3"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 4 }}>
-//           <Image
-//             src="/homepage/partners/4.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 4"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 5 }}>
-//           <Image
-//             src="/homepage/partners/5.png"
-//             width={40}
-//             className="img"
-//             height={40}
-//             alt="Partner 5"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 6 }}>
-//           <Image
-//             src="/homepage/partners/6.png"
-//             width={40}
-//             height={40}
-//             className="img"
-//             alt="Partner 6"
-//           />
-//         </div>
-//         <div className="marquee-item2" style={{ "--item-position": 7 }}>
-//           <Image
-//             src="/homepage/partners/7.png"
-//             width={40}
-//             height={40}
-//             alt="Partner 7"
-//             className="img"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Partnership2;
-import React from "react";
 import { useLocale } from "next-intl";
 import Image from "next/image";
 
 const Partnership2 = () => {
   const locale = useLocale();
-  const isRTL = locale === "en";
+  const isRTL = locale === "ar";
 
   return (
     <div
-      // className={`marquee fadeout-horizontal ${isRTL ? "ltr" : "rtl"}`}
-      className="marquee fadeout-horizontal"
+      className={`marquee fadeout-horizontal ${isRTL ? "rtl" : "ltr"}`}
       style={{ "--num-items": 7 }}
     >
       <div className="marquee-track">
         {[...Array(7)].map((_, index) => (
           <div
             key={index}
-            className="marquee-item2"
+            className="marquee-item"
             style={{
               "--item-position": index + 1,
               animation: `${
-                isRTL ? "marquee-move-rtl" : "marquee-move"
+                isRTL ? "marquee-move-ltr" : "marquee-move-rtl"
               } var(--speed) linear infinite`,
             }}
           >
