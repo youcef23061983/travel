8d8008fe11632fdea0cab7dce5f21cc04897cace
youcef23061983 <youcef.belcourtois@gmail.com>
2025-01-23 00:01:18 +0100

    newchange

--- src/app/[locale]/api/packages/[id]/route.jsx
+++ src/app/[locale]/api/packages/[id]/route.jsx
@@ -5,9 +5,9 @@ export async function GET(request, { params }) {
   const { id } = params;
 
   try {
-    const product = data.find((pro) => pro.id === parent(id));
-    return new Response(JSON.stringify(product));
-    // return NextResponse.json(product);
+    const product = data?.find((pro) => pro.id === id);
+    // return new Response(JSON.stringify(product));
+    return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching package details:", error);
     return NextResponse.json(
